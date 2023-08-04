3a68b56bbbacc351dbfbaca7024080bc6a673749
Pedro Felix777 <pedrofeliix0706@gmail.com>
2023-08-04 14:05:14 -0300

    Simplificando o código com estruturas de repetição

--- Calculadora/javaScript/calculadora.js
+++ Calculadora/javaScript/calculadora.js
@@ -18,3 +18,23 @@ function mult(){
     result.textContent = numberOne.value * numberTwo.value;
 
 }
+
+/* Maneira de usar o if para poupar e facilitar código, sendo if  ou switch case */
+/* Assim, após o onclick devemos colocar o nome da function entre aspas (no caso calculo, e dentro do parenteses e aspas simples devemos por o nome dos parametros) */
+
+/* function calculo (parametros){
+
+    if ( parametros === 'somar'){
+        result.textContent = parseInt(numberOne.value)  + parseInt(numberTwo.value);
+
+    }else if( parametros === 'sub'){
+        result.textContent = numberOne.value - numberTwo.value;
+
+    } else if ( parametros === div ){
+        result.textContent = numberOne.value / numberTwo.value;
+
+    } else if ( parametros === mult ){
+        result.textContent = numberOne.value * numberTwo.value;
+
+}
+} */
