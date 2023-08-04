826067da9f50ebddae2c0560984c8d44c3976eb1
Pedro Felix777 <pedrofeliix0706@gmail.com>
2023-08-04 13:47:28 -0300

    Arrumando código

--- Calculadora/javaScript/calculadora.js
+++ Calculadora/javaScript/calculadora.js
@@ -2,29 +2,19 @@ const numberOne = document.getElementById("numberOne");
 const numberTwo = document.getElementById("numberTwo");
 const result = document.getElementById("result");
 
-let total = 0;
-
 function soma(){
-    
-    
-    
     result.textContent = parseInt(numberOne.value)  + parseInt(numberTwo.value);
-}
 
+}
 function sub(){
-    
-    
     result.textContent = numberOne.value - numberTwo.value;
-}
 
+}
 function div(){
-    
-    
     result.textContent = numberOne.value / numberTwo.value;
+    
 }
-
 function mult(){
-    
-    
     result.textContent = numberOne.value * numberTwo.value;
+
 }
